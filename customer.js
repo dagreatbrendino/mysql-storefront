d5fd502a2fe36e1ddaa11935b1838e112cc8d96a
Brendan K <brendanmkelly94@gmail.com>
2019-03-04 19:40:21 -0600

    added prompts for product id and quantity

--- customer.js
+++ customer.js
@@ -24,7 +24,38 @@ var getProducts = function(){
     connection.query("SELECT * FROM `products` ", function(error, res){
         if (error) throw error;
         console.table(res);
+        promptID();
     });
-    connection.end();
+
 }
+
 getProducts();
+
+
+//prompt user for id of the product they would like to order
+var promptID = function(){
+    inquirer.prompt([
+        {
+            name: "customer_choice",
+            message: "Please enter the ID of the product you wish to order..."
+        }
+    ]).then(function (answer){
+        //store the customers input and pass it 
+        var desired_prod = parseInt(answer.customer_choice);
+        promptQuantity(desired_prod);
+    })
+}
+
+//prompt user for the amount of units they wish to order on that ID
+
+var promptQuantity = function(des_id){
+    inquirer.prompt([
+        {
+            name: "customer_quantity",
+            message: "How much of _____ would you like to order?"
+        }
+    ]).then(function (answer){
+        var quantity = parseInt(answer.customer_quantity);
+        connection.end();
+    })
+}
